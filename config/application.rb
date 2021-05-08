6f2cac67c79e408649a17526ae4c215111c66c21
zvasib <zvasib@ya.ru>
2021-05-08 23:25:18 +0700

    Fix forms devise & fix air datepicker format

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Bbq
     config.load_defaults 6.1
 
     config.i18n.default_locale = :ru
-    config.i18n.locale = :en
+    config.i18n.locale = :ru
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
