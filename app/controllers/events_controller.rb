fd65889518a3006ba8a74470461518d14c16f1d4
zvasib <zvasib@ya.ru>
2021-05-16 17:27:13 +0700

    Add pincode to Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,8 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show]
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
 
+  before_action :password_guard!, only: [:show]
+
   def index
     @events = Event.all
   end
@@ -45,15 +47,34 @@ class EventsController < ApplicationController
   end
 
   private
-    def set_current_user_event
-      @event = current_user.events.find(params[:id])
+
+  def password_guard!
+    return true if @event.pincode.blank?
+    return true if signed_in? && current_user == @event.user
+
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
     end
 
-    def set_event
-      @event = Event.find(params[:id])
-    end
+    pincode = cookies.permanent["events_#{@event.id}_pincode"]
 
-    def event_params
-      params.require(:event).permit(:title, :address, :datetime, :description)
+    unless @event.pincode_valid?(pincode)
+      if params[:pincode].present?
+        flash.now[:alert] = I18n.t('controllers.events.wrong_pincode')
+      end
+      render 'password_form'
     end
+  end
+
+  def set_current_user_event
+    @event = current_user.events.find(params[:id])
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :address, :datetime, :description, :pincode)
+  end
 end
