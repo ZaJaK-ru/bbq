66623663d0cfdb3b87919b437d4fc700d5a5c92f
zvasib <zvasib@ya.ru>
2021-05-19 07:57:53 +0700

    Add ligthbox gallery

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,9 +1,11 @@
 require("@rails/ujs").start()
 import 'bootstrap/dist/js/bootstrap'
 import 'air-datepicker/dist/js/datepicker.min'
+import 'ekko-lightbox/dist/ekko-lightbox.min.js'
 import '@fortawesome/fontawesome-free/css/all'
 import '../scripts/bootstrap' 
 import '../scripts/dp.js'
+import '../scripts/lb.js'
 import '../scripts/map.js'
 
 import '../styles/application'
