2c7d4859004f95b3119cd8a4721b5b30aabc61e4
zvasib <zvasib@ya.ru>
2021-05-19 09:15:16 +0700

    Add event_mailer localization & fix views event_mailer & fix ru.yml->datetime

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,24 +1,23 @@
 class EventMailer < ApplicationMailer
-
   def subscription(event, subscription)
     @email = subscription.user_email
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "Новая подписка на #{ event.title }"
+    mail to: event.user.email, subject: default_i18n_subject(event: event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: "Новый комментарий @ #{ event.title }"
+    mail to: email, subject: default_i18n_subject(event: event.title)
   end
 
   def photo(event, photo, email)
     @photo = photo
     @event = event
 
-    mail to: email, subject: "Новая фотография @ #{ event.title }"
+    mail to: email, subject: default_i18n_subject(event: event.title)
   end
 end
